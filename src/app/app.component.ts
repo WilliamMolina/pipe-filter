e854bca2b04b2b09c144aa21f24461cae73e3139
William Molina <wondercodesoft@gmail.com>
2018-04-02 22:15:14 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FilterPipe} from './filter-pipe.pipe'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title:String;
+  values;
+  constructor(){
+    this.title='Test filter';
+    this.values = [];
+     let stops = [
+      {
+        name:"Stop 1",
+        id : 1,
+        near : [
+          "Exito",
+          "Justo y bueno"
+        ]
+      },
+      {
+        name:"Stop 2",
+        id : 2,
+        near : [
+          "Suramericana",
+          "Metro",
+          "Exito"
+        ]
+      },
+      {
+        name:"Stop 3",
+        id : 2,
+        near : [
+          "Los colores",
+          "Il forno"
+        ]
+      }
+    ];
+    for(let i=0;i<1000;i++){
+      this.values.push.apply(this.values,stops);
+    }
+  }
 }
